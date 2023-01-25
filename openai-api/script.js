141f086d1c8456c7f0a7bb0889c5ed1c237148db
cod3kid <djsufail@gmail.com>
2023-01-25 19:28:02 +0530

    OpenAI API: ChatCompletion Create Method

--- openai-api/script.js
+++ openai-api/script.js
@@ -7,3 +7,12 @@ config();
 const openai = new OpenAI({
   apiKey: process.env.OPEN_AI_API_KEY,
 });
+
+const completion = await openai.chat.completions.create({
+  model: "gpt-4",
+  messages: [
+    { role: "user", content: "Who is the father of nation in India?" },
+  ],
+});
+
+console.log(completion.choices[0]);
