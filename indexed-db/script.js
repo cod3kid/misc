a71e4bdd8adc327aea58762a8b23eaea90f22ac7
cod3kid <djsufail@gmail.com>
2023-02-23 17:00:44 +0530

    Small note

--- indexed-db/script.js
+++ indexed-db/script.js
@@ -1,16 +1,20 @@
 const request = indexedDB.open("notes");
 
-request.onupgradeneeded((e) => {
+request.onupgradeneeded = (e) => {
+  // If the db doesn't exist, this method will be called first and the 'notes' db will
+  // be created
   console.log("On upgrade needed");
-});
+};
 
-request.onsuccess((e) => {
+request.onsuccess = (e) => {
   console.log("On success");
-});
+};
 
-request.onerror((e) => {
+request.onerror = (e) => {
   console.log("On error");
-})(async () => {
+};
+
+(async () => {
   const data = await indexedDB.databases();
   console.log(data);
 })();
--- prac.js
+++ prac.js
@@ -1,18 +1,19 @@
-const addFive = (a) => {
-  return a + 5;
+const myPromiseAll = (promises) => {
+  const result = [];
+
+  return new Promise((resolve, reject) => {
+    promises.forEach((p, index) => {
+      p.then((res) => {
+        result.push(res);
+      }).catch((err) => reject(err));
+
+      if (promises.length === index + 1) {
+        resolve(result);
+      }
+    });
+  });
 };
 
-const subtractTwo = (a) => {
-  return a - 2;
-};
-
-const multiplyFour = (a) => {
-  return a * 4;
-};
-
-const compose = (...fns) => {
-  return (args) => fns.reduceRight((curr, fn) => fn(curr), args);
-};
-
-const composedFn = compose(addFive, subtractTwo, multiplyFour);
-console.log(composedFn(6));
+myPromiseAll([Promise.resolve("Hello"), Promise.resolve("World")]).then((res) =>
+  console.log(res)
+);
