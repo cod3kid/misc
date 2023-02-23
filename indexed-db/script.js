b4671aeeb3cc1af978973f145a59a27c36ca2be0
cod3kid <djsufail@gmail.com>
2023-02-24 00:37:40 +0530

    UI for creating db

--- indexed-db/script.js
+++ indexed-db/script.js
@@ -1,20 +1,25 @@
-const request = indexedDB.open("notes");
+const dbName = document.getElementById("db-name");
+const dbVersion = document.getElementById("db-version");
+const createDbButton = document.getElementById("create-db-button");
 
-request.onupgradeneeded = (e) => {
-  // If the db doesn't exist, this method will be called first and the 'notes' db will
-  // be created
-  console.log("On upgrade needed");
+createDbButton.addEventListener("click", (e) => {
+  createDB(dbName.value, dbVersion.value);
+});
+
+const createDB = (dbName, dbVersion) => {
+  const request = indexedDB.open(dbName, dbVersion);
+
+  request.onupgradeneeded = (e) => {
+    // If the db doesn't exist, this method will be called first and the 'notes' db will
+    // be created
+    console.log("On upgrade needed");
+  };
+
+  request.onsuccess = (e) => {
+    console.log("On success");
+  };
+
+  request.onerror = (e) => {
+    console.log("On error: ", e.target.error);
+  };
 };
-
-request.onsuccess = (e) => {
-  console.log("On success");
-};
-
-request.onerror = (e) => {
-  console.log("On error");
-};
-
-(async () => {
-  const data = await indexedDB.databases();
-  console.log(data);
-})();
