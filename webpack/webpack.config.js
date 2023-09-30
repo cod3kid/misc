47ed7da1d9eab8ceb06646f523f1282ac92399a3
cod3kid <djsufail@gmail.com>
2023-10-01 03:35:24 +0530

    Webpack: Asset Modules

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -5,6 +5,24 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "./dist"),
+    publicPath: "dist/",
   },
   mode: "none",
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 0.5 * 1024 * 1024,
+          },
+        },
+      },
+      {
+        test: /\.(txt)$/,
+        type: "asset/source",
+      },
+    ],
+  },
 };
