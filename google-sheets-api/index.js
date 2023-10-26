8c63fd809ff3a1f16b445ddad0ac8047dea58dbf
cod3kid <djsufail@gmail.com>
2023-10-27 01:53:56 +0530

    Google Sheets Auto Insert Part 4: Connect to Google Doc

--- google-sheets-api/index.js
+++ google-sheets-api/index.js
@@ -1,5 +1,13 @@
 const utilFunctions = require("./utils");
+const doc = require("./sheet");
 const { splitWorkingHoursLineByLine, prepareSheetContents } = utilFunctions;
 
 const workingHours = splitWorkingHoursLineByLine();
 const sheetContents = prepareSheetContents(workingHours);
+
+(async () => {
+  // load the documents info
+  await doc.loadInfo();
+
+  console.log(doc.title);
+})();
---
+++ google-sheets-api/sheet.js
@@ -0,0 +1,17 @@
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+const { JWT, GoogleAuth } = require("google-auth-library");
+require("dotenv").config();
+const CREDENTIALS = require(`./${process.env.GOOGLE_PRIVATE_KEY_FILE_NAME}`);
+
+const serviceAccountAuth = new JWT({
+  email: CREDENTIALS.client_email,
+  key: CREDENTIALS.private_key,
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
+
+const doc = new GoogleSpreadsheet(
+  process.env.GOOGLE_SHEET_ID,
+  serviceAccountAuth
+);
+
+module.exports = doc;
