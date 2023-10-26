18b174563bbc17ac1a31f94cbc09d9c21783511d
cod3kid <djsufail@gmail.com>
2023-10-26 23:19:13 +0530

    Google Sheets Auto Insert Part 3: Prepare sheet contents

--- google-sheets-api/index.js
+++ google-sheets-api/index.js
@@ -1,27 +1,5 @@
 const utilFunctions = require("./utils");
-const { splitWorkingHoursLineByLine } = utilFunctions;
+const { splitWorkingHoursLineByLine, prepareSheetContents } = utilFunctions;
 
 const workingHours = splitWorkingHoursLineByLine();
-console.log(workingHours);
-const inputString = "Itachi 7h 49m 08:59 18:06 9h 37m";
-
-const pattern =
-  /(\S+)\s(\d+h\s\d+m)\s(\d{2}:\d{2})\s(\d{2}:\d{2})\s(\d+h\s\d+m)/;
-
-const matches = pattern.exec(inputString);
-
-if (matches) {
-  const name = matches[1];
-  const duration1 = matches[2];
-  const time1 = matches[3];
-  const time2 = matches[4];
-  const duration2 = matches[5];
-
-  console.log("Name:", name);
-  console.log("Duration 1:", duration1);
-  console.log("Time 1:", time1);
-  console.log("Time 2:", time2);
-  console.log("Duration 2:", duration2);
-} else {
-  console.log("String format doesn't match the expected pattern.");
-}
+const sheetContents = prepareSheetContents(workingHours);
--- google-sheets-api/utils.js
+++ google-sheets-api/utils.js
@@ -30,4 +30,28 @@ const splitWorkingHoursLineByLine = () => {
   return splitWorkingHours;
 };
 
-module.exports = { splitWorkingHoursLineByLine };
+const prepareSheetContents = (workingHours) => {
+  const employeeWorkingHours = [];
+  const pattern =
+    /(\S+)\s(\d+h\s\d+m)\s(\d{2}:\d{2})\s(\d{2}:\d{2})\s(\d+h\s\d+m)/;
+
+  workingHours.forEach((item) => {
+    const matches = pattern.exec(item);
+
+    if (matches) {
+      employeeWorkingHours.push({
+        name: matches[1],
+        duration1: matches[2],
+        time1: matches[3],
+        time2: matches[4],
+        duration2: matches[5],
+      });
+    } else {
+      console.log("String format doesn't match the expected pattern.\n", item);
+    }
+  });
+
+  return employeeWorkingHours;
+};
+
+module.exports = { splitWorkingHoursLineByLine, prepareSheetContents };
