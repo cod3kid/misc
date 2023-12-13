8183f57b1f8c3d940f6fb3612402929db15efda1
cod3kid <djsufail@gmail.com>
2023-12-13 20:03:00 +0530

    Chess: Chessboard

---
+++ chess/app.js
@@ -0,0 +1,89 @@
+const chessBoard = document.getElementById("chessboard");
+const playerTurn = document.getElementById("player");
+const infoDisplay = document.getElementById("info-display");
+const size = 8;
+const initialBoard = [
+  rook,
+  knight,
+  bishop,
+  queen,
+  king,
+  bishop,
+  knight,
+  rook,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  rook,
+  knight,
+  bishop,
+  queen,
+  king,
+  bishop,
+  knight,
+  rook,
+];
+
+const createBoard = () => {
+  initialBoard.forEach((piece, idx) => {
+    const square = document.createElement("div");
+    const row = Math.floor((63 - idx) / 8) + 1;
+    square.setAttribute("square-id", idx);
+    square.classList.add("square");
+
+    if (row % 2 === 0) {
+      square.classList.add(idx % 2 === 0 ? "beige" : "brown");
+    } else {
+      square.classList.add(idx % 2 === 0 ? "brown" : "beige");
+    }
+
+    chessBoard.append(square);
+  });
+};
+
+createBoard();
