8db06c82b251c2062895b81a1635eb74908d23dd
cod3kid <djsufail@gmail.com>
2023-12-15 02:13:41 +0530

    Drag n Drop Perfection

--- drag-n-drop/main.js
+++ drag-n-drop/main.js
@@ -1,14 +1,43 @@
-const card = document.getElementById("card");
+// const card = document.getElementById("card");
+// let isDragging = false;
+
+// card.addEventListener("mousedown", (e) => {
+//   isDragging = true;
+// });
+
+// document.addEventListener("mousemove", (e) => {
+//   if (isDragging) {
+//     card.style.top = e.clientY + "px";
+//     card.style.left = e.clientX + "px";
+//   }
+// });
+
+// document.addEventListener("mouseup", (e) => {
+//   isDragging = false;
+// });
+
+let posX = 0,
+  posY = 0;
 let isDragging = false;
 
+const card = document.getElementById("card");
+
 card.addEventListener("mousedown", (e) => {
+  posX = e.clientX;
+  posY = e.clientY;
   isDragging = true;
 });
 
 document.addEventListener("mousemove", (e) => {
   if (isDragging) {
-    card.style.top = e.clientY + "px";
-    card.style.left = e.clientX + "px";
+    posX = posX - e.clientX;
+    posY = posY - e.clientY;
+
+    card.style.top = card.offsetTop - posY + "px";
+    card.style.left = card.offsetLeft - posX + "px";
+
+    posX = e.clientX;
+    posY = e.clientY;
   }
 });
 
