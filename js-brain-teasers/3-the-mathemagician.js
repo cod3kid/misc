f180ec63febafe5bda909ee693c2f7b888c60a95
cod3kid <djsufail@gmail.com>
2024-04-30 17:33:40 +0530

    JS Brain Teaser - The Mathemagician - Fix

--- js-brain-teasers/3-the-mathemagician.js
+++ js-brain-teasers/3-the-mathemagician.js
@@ -11,3 +11,12 @@ without losing any precision.
 console.log(Number.MIN_SAFE_INTEGER); //  -9007199254740991
 console.log(Number.MAX_SAFE_INTEGER); //  9007199254740991
 console.log(9999999999999999); //  10000000000000000
+
+/* The fix is to use BigInt, Two ways to use BigInt
+ 1. BigInt(100000)
+ 2. 100000n
+*/
+const bLargeNumber = BigInt("10000000000000000");
+const bSmallNumber = 1n;
+
+console.log(bLargeNumber + bSmallNumber);
