c87fd3d55351083a0d4fd0434eb84e7607bebb38
cod3kid <djsufail@gmail.com>
2024-09-01 03:43:09 +0530

    Promise.all polyfill Supports Reject

--- promise-all.js
+++ promise-all.js
@@ -2,20 +2,24 @@ const myPromiseAll = (promises) => {
   const result = [];
 
   return new Promise((resolve, reject) => {
-    promises
-      .forEach((p, index) => {
-        p.then((res) => {
-          result.push(res);
-        });
+    promises.forEach((p, index) => {
+      p.then((res) => {
+        result.push(res);
 
         if (index === promises.length - 1) {
           resolve(result);
         }
-      })
-      .catch((err) => reject(err));
+      }).catch((err) => reject(err));
+    });
   });
 };
 
-myPromiseAll([Promise.resolve("Hello"), Promise.resolve("Return")]).then(
-  (res) => console.log(res)
-);
+myPromiseAll([
+  Promise.resolve("Hello"),
+  Promise.resolve("Return"),
+  Promise.reject("Reject"),
+])
+  .then((res) => console.log(res))
+  .catch((err) => {
+    console.log(err);
+  });
