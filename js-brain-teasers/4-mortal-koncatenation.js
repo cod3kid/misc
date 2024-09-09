e525e9af014491fdf9dceed9088ce2820eccccb5
cod3kid <djsufail@gmail.com>
2024-09-09 17:36:06 +0530

    JS Brain Teaser - Mortal Koncatenation

---
+++ js-brain-teasers/4-mortal-koncatenation.js
@@ -0,0 +1,12 @@
+const femaleMKCharacters = ["Sonya Blade", "Sindel", "Cassie Cage", "Sheeva"];
+
+const maleMKCharacters = ["Scorpion", "Sub-Zero", "Raiden", "Johnny Cage"];
+
+const MKCharacters = femaleMKCharacters + maleMKCharacters;
+console.log(MKCharacters);
+
+/*
+Sonya Blade,Sindel,Cassie Cage,SheevaScorpion,Sub-Zero,Raiden,Johnny Cage
+
+JS treats them as strings and concatenates the string.
+*/
