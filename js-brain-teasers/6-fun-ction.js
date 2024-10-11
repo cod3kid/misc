179425645815ea380b5745ccba7792c730877394
cod3kid <djsufail@gmail.com>
2024-10-11 17:46:00 +0530

    JS Brain Teaser - Fun-ction -  Different forms of IIFE

--- js-brain-teasers/6-fun-ction.js
+++ js-brain-teasers/6-fun-ction.js
@@ -1,7 +1,36 @@
-!function() {
-    const name = "john";
-    const age = 20;
-}();
+!(function () {
+  const name = "john";
+  const age = 20;
+})();
 
 const capitalizedName = name[0].toUpperCase() + name.slice(1);
-console.log(capitalizedName);
+console.log(capitalizedName); // Reference Error
+
+/*
+Different forms of IIFE
+*/
+
+// Traditional Enclosure
+(function () {
+  console.log("meow");
+})();
+
+// Traditional with Arrow Fn Enclosure
+(() => {
+  console.log("meow");
+})();
+
+// Unary Operator Enclosure
+!(function () {
+  console.log("meow");
+})();
+
+// Grouping Enclosure
+(function () {
+  console.log("meow");
+})();
+
+// Function Declaration Enclosure
+(function namedIIFE() {
+  console.log("meow");
+})();
