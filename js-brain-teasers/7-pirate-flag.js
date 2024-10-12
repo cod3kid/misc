652cc951fc30eefbe207c56231428add7bf275d8
cod3kid <djsufail@gmail.com>
2024-10-12 19:34:53 +0530

    JS Brain Teaser - Pirate Flag - ZWJ

---
+++ js-brain-teasers/7-pirate-flag.js
@@ -0,0 +1,11 @@
+const flag = "🏴‍☠";
+console.log(flag.length);
+
+/*
+ 🏴 . ☠
+ The flag is formed by two sequence, the flag and skull connected with  a dot. This is called ZWJ 
+ sequence (Zero Width Joiner)
+
+ The black flag is formed of two unicode characters and skull by 1 and the dot
+ So total 4
+*/
