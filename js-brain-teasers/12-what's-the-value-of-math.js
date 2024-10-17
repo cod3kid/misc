313248fab24da55994bb316ca6624024dafae5b8
cod3kid <djsufail@gmail.com>
2024-10-17 22:17:46 +0530

    JS Brain Teaser - What's the value of Math - Small Comment

--- js-brain-teasers/12-what's-the-value-of-math.js
+++ js-brain-teasers/12-what's-the-value-of-math.js
@@ -13,3 +13,9 @@ const r = Math.random,
   a = [(r() * 11) | 0, (r() * 11) | 0, (r() * 11) | 0];
 
 console.log(a);
+
+/*
+First, we get a random number between 0 and 1, We'll mutiply into 11 to get a number
+between 0 and 11, Now when we Bitwise OR it, it'll convert into integer first and then perform
+the Bitwise OR operation.
+*/
