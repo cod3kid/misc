271111901b82506b825c821b6a0e704607d4066d
cod3kid <djsufail@gmail.com>
2024-10-19 22:22:59 +0530

    JS Brain Teaser - The Permanent Closure - let

--- js-brain-teasers/14-the-permanent-closure.js
+++ js-brain-teasers/14-the-permanent-closure.js
@@ -10,3 +10,39 @@ function createCounterArray() {
 
 const counters = createCounterArray();
 counters.forEach((counter) => counter());
+
+/*
+Output
+5
+5
+5
+5
+5
+
+This result is because var has function-level scope, not block-level scope.
+The variable i declared in the for loop is shared across all the closure functions.
+By the time the functions are called, the for loop has already finished executing, and the final value of i is 5.
+*/
+
+/*
+Output
+0
+1
+2
+3
+4
+
+function createCounterArray() {
+  const counterArray = [];
+  for (let i = 0; i < 5; i++) {
+    counterArray.push(() => {
+      console.log(`Counter: ${i}`);
+    });
+  }
+  return counterArray;
+}
+
+const counters2 = createCounterArray();
+counters2.forEach((counter) => counter());
+
+*/
