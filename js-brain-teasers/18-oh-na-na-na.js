ae1d5eb154ba0b1eafb2c127b2237e263fd5eea4
cod3kid <djsufail@gmail.com>
2024-10-23 23:49:23 +0530

    JS Brain Teaser -

---
+++ js-brain-teasers/18-oh-na-na-na.js
@@ -0,0 +1,9 @@
+let price = 10;
+let tax;
+
+const sum = price + tax;
+console.log(sum); // NaN
+
+/* sqrt of negative number, integer math with Infinity etc will result in NaN
+ NaN === NaN is false
+*/
