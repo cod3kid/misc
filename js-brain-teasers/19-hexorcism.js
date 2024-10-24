544b4733daaee472e9cde8bb6e18f61f6f7f3e16
cod3kid <djsufail@gmail.com>
2024-10-24 23:53:10 +0530

    JS Brain Teaser - Hexocism

---
+++ js-brain-teasers/19-hexorcism.js
@@ -0,0 +1,12 @@
+const hex = "0x1E";
+
+console.log(hex - 0);
+
+const hexNumber = "B2F";
+const decimalNumber = parseInt(hexNumber, 16);
+
+console.log(decimalNumber); // → 2863
+
+console.log(0xf); // Hex 15
+console.log(0o11); // Octal 9
+console.log(0b1000); // Binary 8
