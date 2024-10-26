9615f5323b9a71da734b000a0c4a2ae5185f0f67
cod3kid <djsufail@gmail.com>
2024-10-27 00:52:07 +0530

    JS Brain Teaser - Alphabet Aerobics

---
+++ js-brain-teasers/23-alphabet-aerobics.js
@@ -0,0 +1,7 @@
+for (i = 9, a = ""; ++i < 36; ) a += i.toString(36);
+console.log(a);
+
+/*
+Base 36 is a numeral system that uses 36 distinct symbols to represent numbers:
+the digits 0–9 and the letters A–Z (or a–z, depending on convention).
+*/
