c7b914c218ee508a09c73aba16c61c5860c4251f
cod3kid <djsufail@gmail.com>
2024-10-31 01:17:27 +0530

    JS Brain Teaser - The Grocery List

---
+++ js-brain-teasers/27-the-grocery-list.js
@@ -0,0 +1,12 @@
+const groceryList = [];
+
+groceryList[0] = "Bread";
+groceryList[1] = "Milk";
+groceryList["user"] = "John";
+
+console.log(groceryList.length);
+
+/*
+Outputs 2 because user is just a property we attach to the array,
+it' not an element of the array
+*/
